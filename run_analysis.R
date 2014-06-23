afc507ea9d2c5d205a4c4a6255d61d2c34ec9ef9
hanumansetty <hanumansetty@users.noreply.github.com>
2014-06-23 01:35:48 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -3,7 +3,6 @@
 # c3w3p3Url<-"https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
 # download.file(c3w3p3Url,"./Dataset.zip",mode="wb")
 
-# Read raw test data
 testRawDf<-read.table("./X_test.txt")
 testLablesDf<-read.table("./y_test.txt")
 
@@ -19,8 +18,17 @@ cNames<-read.table("./features.txt")
 names(testRawDf)<-cNames[,2]
 names(trainRawDf)<-cNames[,2]
 
-#1 Merges the training and the test sets to create one data set.
+#1. Merges the training and the test sets to create one data set.
 allRawDataDf<-rbind(testRawDf,trainRawDf)
+
+#to get the index of columns of interest
+index=1:length(allRawDataDf)
+meanIndex<-index%in%grep("-mean()",names(allRawDataDf))
+stdIndex<-index%in%grep("-std()",names(allRawDataDf))
+relevantCols<-as.logical(meanIndex+stdIndex)
+
+#2. Extracts only the measurements on the mean and standard deviation for each measurement
+allRawDataDf<-allRawDataDf[,relevantCols]
 
 # set activityId as the variable Name for the lables
 names(testLablesDf)<-c("activityId")
